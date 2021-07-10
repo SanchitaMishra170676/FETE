e2240e135df980f5ae3e59abd09a7e7674816d8a
Sanchita Mishra <59258203+SanchitaMishra170676@users.noreply.github.com>
2021-07-10 22:39:04 +0530

    debug-true

--- fete/settings.py
+++ fete/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m&+d@9(m(%l7cfw@z0yjw2bch*44^2&xd=j@se5bu9zwd66)@r'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
